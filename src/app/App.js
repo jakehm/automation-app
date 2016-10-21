16ba86df1ea3d0657ac4dfb5ad368d114b94eeab
Jacob Harrow-Mortelliti <jacob.harrowmortelliti@maine.edu>
2016-10-21 17:17:16 -0400

    first commit

--- src/app/App.js
+++ src/app/App.js
@@ -1,15 +1,13 @@
 import React from 'react';
-// import 'react-toolbox/lib/commons.scss';           // Import common styles
+//import 'react-toolbox/lib/commons.scss';           // Import common styles
 import PurpleAppBar from './PurpleAppBar.js';      // AppBar with simple overrides
-import SuccessButton from './SuccessButton.js';    // A button with complex overrides
 import { Button } from 'react-toolbox/lib/button'; // Bundled component import
 
 const App = () => (
   <div>
     <PurpleAppBar />
     <section style={{ padding: 20 }}>
-      <SuccessButton label='Success' primary raised />
-      <Button label='Primary Button' primary />
+      <Button label='go' primary raised/>
     </section>
   </div>
 );
