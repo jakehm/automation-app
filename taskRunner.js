e5c73169b654e03297b84932421ef1343239eb0d
Jacob Harrow-Mortelliti <jacob.harrowmortelliti@maine.edu>
2016-10-27 06:39:36 -0400

    webdriverjs not working headless

--- taskRunner.js
+++ taskRunner.js
@@ -6,8 +6,8 @@ const driver = new webdriver.Builder()
 	.forBrowser('firefox')
 	.build()
 
-driver.get('http://www.google.com/ncr');
-driver.findElement(By.name('q')).sendKeys('webdriver');
-driver.findElement(By.name('btnG')).click();
-driver.wait(until.titleIs('webdriver - Google Search'), 1000);
+driver.get('http://www.bing.com');
+driver.getTitle((title) => {
+  console.log(title)
+})
 driver.quit();
