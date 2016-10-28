42ddf2e9f0fbc1dd23f77f5b86af5e99f123cb98
Jacob Mortelliti <jacob.harrowmortelliti@maine.eu>
2016-10-28 08:21:20 -0400

    trying to get chained promises to work

--- taskRunner.js
+++ taskRunner.js
@@ -1,42 +1,58 @@
 const Horseman = require('node-horseman')
-const horseman = new Horseman()
+const horseman = new Horseman({ignoreSSLErrors: true})
+const q = require('q')
 
 const testData = [
   {action: 'goToUrl', target: 'http://www.bing.com'},
   {action: 'doText', target: '#sb_form_q',  textInput: 'hi'}
 ]
-
-const doTasks = tasks => {
-  let taskIndex = 0;
-  const next = () => {
-    if (taskIndex < tasks.length) {
-      let task = tasks[taskIndex]
-      taskIndex++
-      let horsemanAction
-      switch (task.action) {
-        case 'goToUrl':
-          horsemanAction = horseman.open(task.target)
-        case 'doClick':
-          horsemanAction = horseman.click(task.target)
-        case 'doText':
-          horsemanAction = horseman.type(task.target, task.textInput)
-      }
-      return horsemanAction.wait(5000).then(next)  
-    } else {
-      return horseman
-    }
-  } 
-  return next()
+const processTask = task => {
+	console.log("processing task: "+task)
+	let horsemanAction;
+	switch (task.action) {
+    case 'goToUrl':
+      horsemanAction = horseman.open(task.target)
+    case 'doClick':
+      horsemanAction = horseman.click(task.target)
+    case 'doText':
+      horsemanAction = horseman.type(task.target, task.textInput)
+  }
+  return horsemanAction.wait(5000).title().then((title) => {console.log(title)})
 }
 
+const chain = testData.reduce((previous, item) => {
+	return previous.then((previousValue) =>{
+		processTask(previousValue)
+	})
+}, q.resolve("start"))
 
-const taskRunner = tasks => {
+
+// let taskIndex = 1;
+
+// const doTasks = tasks => {
+//   return new Promise((resolve, reject) => {
+//   	readyActions[0].then( () => {
+// 			if (taskIndex < tasks.length) {
+// 	      let task = tasks[taskIndex]
+// 	      taskIndex++
+// 	      return task.then(doTasks(tasks))  
+// 	  	}
+// 	  }).then(resolve)
+//   })
+// }
+
+const taskRunner = () => {
   horseman
     .userAgent('Mozilla/5.0 (Windows NT 6.1; WOW64; rv:27.0) Gecko/20100101 Firefox/27.0')
-    .then(doTasks(tasks))
+    .then(chain.then((lastResult) => processTask(lastResult)))
+    // .open('http://www.bing.com')
+    // .type('#sb_form_q', 'hi')
+    // .title().then((title)=> {console.log(title)})
     .finally(() => {
-      horseman.close()
+    	horseman.close()
     })
 }
 
-taskRunner(testData)
+taskRunner()
+
+
