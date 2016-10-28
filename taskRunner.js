a96654e09f0233163111eef8f96b352bc9a51038
Jacob Harrow-Mortelliti <jacob.harrowmortelliti@maine.edu>
2016-10-28 12:53:04 -0400

    taskrunner is working

--- taskRunner.js
+++ taskRunner.js
@@ -1,58 +1,44 @@
 const Horseman = require('node-horseman')
 const horseman = new Horseman({ignoreSSLErrors: true})
-const q = require('q')
 
 const testData = [
   {action: 'goToUrl', target: 'http://www.bing.com'},
   {action: 'doText', target: '#sb_form_q',  textInput: 'hi'}
 ]
-const processTask = task => {
-	console.log("processing task: "+task)
-	let horsemanAction;
-	switch (task.action) {
+
+const taskFactory = task => {
+  let horsemanAction;
+  switch (task.action) {
     case 'goToUrl':
-      horsemanAction = horseman.open(task.target)
+      horsemanAction = () =>  horseman.open(task.target)
+      break;
     case 'doClick':
-      horsemanAction = horseman.click(task.target)
+      horsemanAction = () => horseman.click(task.target)
+      break;
     case 'doText':
-      horsemanAction = horseman.type(task.target, task.textInput)
+      horsemanAction = () => horseman.type(task.target, task.textInput)
+      break;
   }
-  return horsemanAction.wait(5000).title().then((title) => {console.log(title)})
+  return horsemanAction
 }
 
-const chain = testData.reduce((previous, item) => {
-	return previous.then((previousValue) =>{
-		processTask(previousValue)
-	})
-}, q.resolve("start"))
+const executeTasks = (tasks) => {
+  let result = Promise.resolve()
+  tasks.forEach(task => {
+    result = result.then( taskFactory(task) )
+  })
+  return result
+} 
 
-
-// let taskIndex = 1;
-
-// const doTasks = tasks => {
-//   return new Promise((resolve, reject) => {
-//   	readyActions[0].then( () => {
-// 			if (taskIndex < tasks.length) {
-// 	      let task = tasks[taskIndex]
-// 	      taskIndex++
-// 	      return task.then(doTasks(tasks))  
-// 	  	}
-// 	  }).then(resolve)
-//   })
-// }
-
-const taskRunner = () => {
+const taskRunner = (tasks) => {
   horseman
     .userAgent('Mozilla/5.0 (Windows NT 6.1; WOW64; rv:27.0) Gecko/20100101 Firefox/27.0')
-    .then(chain.then((lastResult) => processTask(lastResult)))
-    // .open('http://www.bing.com')
-    // .type('#sb_form_q', 'hi')
-    // .title().then((title)=> {console.log(title)})
-    .finally(() => {
-    	horseman.close()
-    })
+    .then(() => executeTasks(tasks))
+    .screenshot('test.png')
+    .close()
+    .catch(console.log.bind(console))
 }
 
-taskRunner()
+taskRunner(testData)
 
 
